e6214ec603bfd44acc2f442ce51ab511a8e6fc5b
paco <91361166+NqmaSvobodnoIme@users.noreply.github.com>
2023-11-29 13:21:32 +0200

    Add files via upload

---
+++ CalculationSystem/CalculationSystem/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculationSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] inputNumbers = textBox1.Text.Split(' ');
+
+            List<double> numbers = new List<double>();
+            foreach (string input in inputNumbers)
+            {
+                if (double.TryParse(input, out double number))
+                {
+                    numbers.Add(number);
+                }
+            }
+
+            double moda = Mode(numbers);
+            double Average = numbers.Average();
+            mode.Text = $"Mode: {moda}";
+            average.Text = $"Average: {Average}";
+        }
+        private double Mode(List<double> numbers)
+        {
+            var mode = numbers.GroupBy(x => x)
+                 .OrderByDescending(x => x.Count()).ThenBy(x => x.Key)
+                 .Select(x => (int)x.Key)
+                 .FirstOrDefault();
+
+            return mode;
+        }
+    }
+}
